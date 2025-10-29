110f7613b34002ab9f4cd1f0519ec2f77e8ef5d3
Bohdan Boklah <b.boklag@gmail.com>
2025-10-30 00:33:29 +0200

    remove config YAML file

--- scripts/downloading_datasets.py
+++ scripts/downloading_datasets.py
@@ -1,23 +1,22 @@
 import os
 import httpx
-import yaml
 
 
-async def download_dataset(config_path: str, save_dir: str) -> None:
+async def download_dataset(
+        save_dir: str,
+        url_dataset: str,
+        dataset_name: str
+) -> None:
     os.makedirs(save_dir, exist_ok=True)
-    with open(config_path, "r", encoding="utf-8") as f:
-        config = yaml.safe_load(f)
-    dataset_name = tuple(config['datasets'].keys())[0]
-    csv_url = config['datasets'][dataset_name]
     save_path = os.path.join(save_dir, f"{dataset_name}.csv")
     async with httpx.AsyncClient(timeout=None) as client:
         try:
-            r = await client.get(csv_url)
+            r = await client.get(url_dataset)
             r.raise_for_status()
             with open(save_path, "wb") as f:
                 f.write(r.content)
         except httpx.HTTPError as e:
             print(
-                f"❌ Failed to download CSV file '{dataset_name}' from {csv_url}."
+                f"❌ Failed to download CSV file '{dataset_name}' from {url_dataset}. "
                 f"The file was not saved to {save_path}. Reason: {e}"
             )

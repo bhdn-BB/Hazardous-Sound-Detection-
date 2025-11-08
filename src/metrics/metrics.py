28ea3f23ff3c71d4cb5d1372f1dda90782729c9e
Bohdan Boklah <b.boklag@gmail.com>
2025-11-08 23:31:18 +0200

    implement calculating mains metrics

---
+++ src/metrics/metrics.py
@@ -0,0 +1,9 @@
+import torchmetrics
+
+KEY2METRICS = {
+    "f1" : torchmetrics.F1Score,
+    'recall':torchmetrics.Recall,
+    'precision':torchmetrics.Precision,
+    'accuracy':torchmetrics.Accuracy,
+    'rocauc': torchmetrics.AUROC
+}
